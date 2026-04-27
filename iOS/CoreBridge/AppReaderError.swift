899c7dd0b6ab4f8ae1e3e64f656bb0162dc430b3
Minliny <minliny@MinlinydeMacBook-Air.local>
2026-04-27 23:09:27 +0800

    fix: remove unused ReaderCoreErrorMapper and simplify AppReaderError

--- iOS/CoreBridge/AppReaderError.swift
+++ iOS/CoreBridge/AppReaderError.swift
@@ -31,32 +31,6 @@ public struct AppReaderError: Error, Sendable {
         self.stage = stage
         self.underlyingError = underlyingError
     }
-
-    public static func from(readerError: ReaderError) -> AppReaderError {
-        let code: Code
-        switch readerError.code {
-        case .unknown:
-            code = .unknown
-        case .network:
-            code = .network
-        case .parser:
-            code = .parser
-        case .timeout:
-            code = .timeout
-        case .notFound:
-            code = .notFound
-        case .invalidResponse:
-            code = .invalidResponse
-        default:
-            code = .unknown
-        }
-        return AppReaderError(
-            code: code,
-            message: readerError.message,
-            stage: nil,
-            underlyingError: nil
-        )
-    }
 }
 
 extension AppReaderError: CustomStringConvertible {
